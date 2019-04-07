64efaf2e3c7fce0ab3bfced0ecb557aa7b965831
Eric <eleon23@uic.edu>
2019-04-07 14:58:44 -0500

    First commit

---
+++ app/src/main/java/com/example/eleon/cs478project0/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.eleon.cs478project0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "Eric Leon was here!");
+
+    }
+}
